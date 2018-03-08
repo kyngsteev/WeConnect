6c5844df3f0affece5afaefea0b201a90a51496e
kyngsteev <stephenomoarukhe@gmail.com>
2018-03-08 23:26:08 +0100

    Added business models to businessModel.js

---
+++ Server/api/models/businessModel.js
@@ -0,0 +1,19 @@
+module.exports = [
+	{
+		id: 1,
+		bizName: 'ABC Transport Limited',
+		bizAddress: '34 XYZ street, Lagos'
+	},
+
+	{
+		id: 2,
+		bizName: 'Iya Photo Images',
+		bizAddress: '1 Cyclone Causeway, Jos'
+	},
+
+	{
+		id: 3,
+		bizName: 'Emeka Metal Works',
+		bizAddress: '55, Jabulani Cresent, Enugu'
+	}
+];
