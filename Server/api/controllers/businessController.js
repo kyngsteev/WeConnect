4ad87bb9861c009fdd6579c150c885d266debbeb
kyngsteev <stephenomoarukhe@gmail.com>
2018-03-11 11:39:01 +0100

    added CRUD endpoints in controller

--- Server/api/controllers/businessController.js
+++ Server/api/controllers/businessController.js
@@ -8,12 +8,12 @@ let api = express.Router();
 // '/v1/businesses' - Create
 api.post('/', (req, res) => {
 	if (!req.body.name) {
-		return res.status(404).json({
+		return res.status(400).json({
 			message: 'Bad Request: name missing',
 			error: true
 		});
 	} else if (!req.body.address) {
-		return res.status(404).json({
+		return res.status(400).json({
 			message: 'Bad Request: address missing',
 			error: true
 		});
@@ -22,61 +22,61 @@ api.post('/', (req, res) => {
 	return res.status(201).json(newBiz);
 });
 
-// '/v1/restaurant/' - Read
+// '/v1/businesses' - Read
 api.get('/', (req, res) => res.json({
-	restaurants,
+	dummyModels,
 	error: false
 }));
 
-// // '/v1/restaurant/:restaurantid' - Get 1 record
-// api.get('/:restaurantid', (req, res) => {
-// 	for (let restaurant of restaurants) {
-// 		if (restaurant.id === parseInt(req.params.restaurantid, 10)) {
-// 			return res.json({
-// 				restaurant,
-// 				message: 'success',
-// 				error: false
-// 			});
-// 		}
-// 	}
-// 	return res.status(404).json({
-// 		message: 'User not found',
-// 		error: true
-// 	});
-// });
+// '/v1/businesses/:businessId' - Get 1 record
+api.get('/:businessId', (req, res) => {
+	for (let dummyData of dummyModels) {
+		if (dummyData.id === parseInt(req.params.businessId, 10)) {
+			return res.json({
+				dummyData,
+				message: 'success',
+				error: false
+			});
+		}
+	}
+	return res.status(404).json({
+		message: 'User not found',
+		error: true
+	});
+});
 
-// // '/v1/restaurant/:restaurantid' - Update
-// api.put('/:restaurantid', (req, res) => {
-// 	for (let restaurant of restaurants) {
-// 		if (restaurant.id === parseInt(req.params.restaurantid, 10)) {
-// 			restaurant.name = req.body.name;
-// 			return res.json({
-// 				message: 'success',
-// 				error: false
-// 			});
-// 		}
-// 	}
-// 	return res.status(404).json({
-// 		message: 'User not found',
-// 		error: true
-// 	});
-// });
+// '/v1/businesses/:businessId' - Update
+api.put('/:businessId', (req, res) => {
+	for (let dummyData of dummyModels) {
+		if (dummyData.id === parseInt(req.params.businessId, 10)) {
+			dummyData.bizName = req.body.name;
+			return res.json({
+				message: 'success',
+				error: false
+			});
+		}
+	}
+	return res.status(404).json({
+		message: 'User not found',
+		error: true
+	});
+});
 
-// // '/v1/restaurant/:restaurantid' - Delete
-// api.delete('/:restaurantid', (req, res) => {
-// 	for (let i = 0; i < restaurants.length; i++) {
-// 		if (restaurants[i].id === parseInt(req.params.restaurantid, 10)) {
-// 			restaurants.splice(i, 1);
-// 			return res.json({
-// 				message: 'success',
-// 				error: false
-// 			});
-// 		}
-// 	}
-// 	return res.status(404).json({
-// 		message: 'User not found',
-// 		error: true
-// 	});
-// });
+// '/v1/businesses/:businessId' - Delete
+api.delete('/:businessId', (req, res) => {
+	for (let i = 0; i < dummyModels.length; i += 1) {
+		if (dummyModels[i].id === parseInt(req.params.businessId, 10)) {
+			dummyModels.splice(i, 1);
+			return res.json({
+				message: 'success',
+				error: false
+			});
+		}
+	}
+	return res.status(404).json({
+		message: 'User not found',
+		error: true
+	});
+});
 
 module.exports = api;
--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(require('./routes'));
+app.use(require('./api/routes/businessRoutes'));
 
 app.listen('3000', () => {
 	console.log('App running on port 3000');
