23e85cf82b2f6eb80855b53944408f8000f181fd
kyngsteev <stephenomoarukhe@gmail.com>
2018-03-12 10:43:50 +0100

    updated dummy

--- Server/api/models/dummyData.js
+++ Server/api/models/dummyData.js
@@ -2,19 +2,43 @@ let bizModels = [
 	{
 		id: 1,
 		bizName: 'ABC Transport Limited',
-		bizAddress: '34 XYZ street, Lagos'
+		bizAddress: '34 XYZ street, Lagos',
+		location: 'Lagos',
+		category: 'Transprotation',
+		review: [
+			{
+				title: 'Good Service',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+			}
+		]
 	},
 
 	{
 		id: 2,
 		bizName: 'Iya Photo Images',
-		bizAddress: '1 Cyclone Causeway, Jos'
+		bizAddress: '1 Cyclone Causeway, Jos',
+		location: 'Jos',
+		category: 'Photography',
+		review: [
+			{
+				title: 'Good Service',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+			}
+		]
 	},
 
 	{
 		id: 3,
 		bizName: 'Emeka Metal Works',
-		bizAddress: '55, Jabulani Cresent, Enugu'
+		bizAddress: '55, Jabulani Cresent, Enugu',
+		location: 'Enugu',
+		category: 'Metal Craft',
+		review: [
+			{
+				title: 'Good Service',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+			}
+		]
 	}
 ];
 
