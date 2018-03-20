485b50ad60699af00dc6e7c5b6179a0756aaa2ff
kyngsteev <stephenomoarukhe@gmail.com>
2018-03-20 18:02:43 +0100

    commit swagger

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
+
+let swaggerDefinition = {
+	info: {
+		title: 'Node Swagger API',
+		version: '1.0.0',
+		description: 'Demonstrating how to describe a RESTful API with Swagger',
+	},
+	host: 'localhost:3000',
+	basePath: '/',
+};
+
+// options for the swagger docs
+let options = {
+	// import swaggerDefinitions
+	swaggerDefinition,
+	// path to the API docs
+	apis: ['./api/routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+let swaggerSpec = swaggerJSDoc(options);
+
+// serve swagger
+app.get('/swagger.json', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(swaggerSpec);
+});
 
 app.use(bodyParser.json());
 
