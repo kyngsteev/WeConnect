513ee318f401015d94084ec9007e5dfdeb662360
kyngsteev <stephenomoarukhe@gmail.com>
2018-03-29 04:31:55 +0100

    added bodyParser to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('./server/routes/appRoutes'));
 
